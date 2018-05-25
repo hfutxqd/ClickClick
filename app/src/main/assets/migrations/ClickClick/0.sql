2f19c3e13777f764dc64986736efcd75380be952
徐啟东 <xuqidong@meizu.com>
2018-05-25 12:45:18 +0800

    ActionFunction

--- app/src/main/assets/migrations/ClickClick/0.sql
+++ app/src/main/assets/migrations/ClickClick/0.sql
@@ -3,6 +3,7 @@ INSERT INTO defined_function (name, body, description) values ('打开QQ', 'url:
 INSERT INTO defined_function (name, body, description) values ('打开微信', 'url:weixin://', '打开微信');
 INSERT INTO defined_function (name, body, description) values ('打开支付宝', 'url:alipay:// ', '一键打开支付宝');
 INSERT INTO defined_function (name, body, description) values ('支付宝付款', 'url:alipays://platformapi/startapp?appId=20000056', '打开支付宝付款界面');
-INSERT INTO defined_function (name, body, description) values ('打开相机', 'action:android.media.action.STILL_IMAGE_CAMERA', '打开相机');
+INSERT INTO defined_function (name, body, description) values ('打开相机', 'action:action://android.media.action.STILL_IMAGE_CAMERA', '打开相机');
 INSERT INTO defined_function (name, body, description) values ('打开录像', 'url:android.media.action.VIDEO_CAMERA', '打开录像');
 INSERT INTO defined_function (name, body, description) values ('测试微信扫一扫', 'internal:wechat_scan', '打开微信扫一扫');
+INSERT INTO defined_function (name, body, description) values ('测试微信扫一扫Intent', 'action:intent://#Intent;component=com.tencent.mm/.ui.LauncherUI;B.LauncherUI.From.Scaner.Shortcut=true;end', '打开微信扫一扫');
--- app/src/main/java/xyz/imxqd/clickclick/func/ActionFunction.java
+++ app/src/main/java/xyz/imxqd/clickclick/func/ActionFunction.java
@@ -1,8 +1,11 @@
 package xyz.imxqd.clickclick.func;
 
 import android.content.Intent;
+import android.text.TextUtils;
 
 import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 import xyz.imxqd.clickclick.App;
 
@@ -21,10 +24,42 @@ public class ActionFunction extends AbstractFunction {
         return new JSONObject();
     }
 
+    private boolean isPureIntent(String args) {
+        if (TextUtils.isEmpty(args) || args.length() < 10) {
+            return false;
+        }
+        return args.startsWith("intent://");
+    }
+
+    public Intent getPureIntent(String args) {
+        try {
+            return Intent.parseUri(args.substring(9), 0);
+        } catch (URISyntaxException e) {
+            return new Intent();
+        }
+    }
+
+    private boolean isActionIntent(String args) {
+        if (TextUtils.isEmpty(args) || args.length() < 10) {
+            return false;
+        }
+        return args.startsWith("action://");
+    }
+
+    private Intent getActionIntent(String args) {
+        return new Intent(args.substring(9));
+    }
+
     @Override
     public void doFunction(String args) {
-        Intent intent = new Intent(getAction(args));
-        App.get().startActivity(intent);
-
+        Intent intent = null;
+        if (isActionIntent(args)) {
+            intent = getActionIntent(args);
+        } else if (isPureIntent(args)) {
+            intent = getPureIntent(args);
+        }
+        if (intent != null) {
+            App.get().startActivity(intent);
+        }
     }
 }
