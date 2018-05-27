f94d1612ad0ec9dfc551e6a5e4e1520cb4c440d2
IMXQD <ahtlxqd@gmail.com>
2018-05-27 23:25:35 +0800

    add all keycode supported by Android

--- app/src/main/java/xyz/imxqd/clickclick/model/web/RemoteFunction.java
+++ app/src/main/java/xyz/imxqd/clickclick/model/web/RemoteFunction.java
@@ -1,6 +1,12 @@
 package xyz.imxqd.clickclick.model.web;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.imxqd.clickclick.utils.KeyEventUtil;
 
 public class RemoteFunction {
 
@@ -24,4 +30,23 @@ public class RemoteFunction {
 
     @SerializedName("update_time")
     public long updateTime;
+
+    public static void main(String ... args) {
+        List<RemoteFunction> list = new ArrayList<>();
+        for (int i = 1; i < 284; i++) {
+            String name = KeyEventUtil.getKeyName(i);
+            RemoteFunction f = new RemoteFunction();
+            f.name = "按键" + name;
+            f.author = "IMXQD";
+            f.body = "keyevent:" + i;
+            f.description = "按键" + name;
+            f.updateTime = 1527414422000L;
+            f.versionCode = 1;
+            f.versionName = "1.0";
+            list.add(f);
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        System.out.println(json);
+    }
 }
