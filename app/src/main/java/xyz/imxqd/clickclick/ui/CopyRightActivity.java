ab9a5c0afd8d376d0b0831ce0cabc36b16b768f0
XuQidong <xuqidong@meizu.com>
2018-06-01 14:09:07 +0800

    add License dialog

---
+++ app/src/main/java/xyz/imxqd/clickclick/ui/CopyRightActivity.java
@@ -0,0 +1,34 @@
+package xyz.imxqd.clickclick.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import xyz.imxqd.clickclick.BuildConfig;
+import xyz.imxqd.clickclick.R;
+
+public class CopyRightActivity extends AppCompatActivity {
+
+    @BindView(R.id.copy_webview)
+    WebView mWebView;
+    @BindView(R.id.copy_title)
+    TextView mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_copy_right);
+        ButterKnife.bind(this);
+        mTitle.setText(getString(R.string.app_name) + " " + BuildConfig.VERSION_NAME);
+        mWebView.loadUrl("file:///android_asset/LICENSE.txt");
+    }
+
+    @OnClick(R.id.copy_ok)
+    public void onOkayClick() {
+        finish();
+    }
+}
