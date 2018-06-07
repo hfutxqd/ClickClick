1dd80c82ed7a63f5507f4d79f49bfc6a34925d97
XuQidong <xuqidong@meizu.com>
2018-06-07 12:40:58 +0800

    fix bug: start activity action of Home button has a 5s delay

--- app/src/main/java/xyz/imxqd/clickclick/model/AppEventManager.java
+++ app/src/main/java/xyz/imxqd/clickclick/model/AppEventManager.java
@@ -233,7 +233,7 @@ public class AppEventManager implements KeyEventHandler.Callback {
         mToast.show();
     }
 
-    public IFunction getHomeDoubleClickFunction(int button) {
+    public static IFunction getHomeDoubleClickFunction(int button) {
         KeyMappingEvent event = new Select()
                 .from(KeyMappingEvent.class)
                 .where(KeyMappingEvent_Table.enable.eq(true))
@@ -247,7 +247,7 @@ public class AppEventManager implements KeyEventHandler.Callback {
         return null;
     }
 
-    public IFunction getHomeTripleClickFunction(int button) {
+    public static IFunction getHomeTripleClickFunction(int button) {
         KeyMappingEvent event = new Select()
                 .from(KeyMappingEvent.class)
                 .where(KeyMappingEvent_Table.enable.eq(true))
@@ -289,19 +289,29 @@ public class AppEventManager implements KeyEventHandler.Callback {
             @Override
             public void onDoubleClick(int button) {
                 LogUtils.d("onDoubleClick " + button);
-                IFunction f = getHomeDoubleClickFunction(button);
-                if (f != null) {
-                    f.exec();
-                }
+                App.get().getHandler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        IFunction f = getHomeDoubleClickFunction(button);
+                        if (f != null) {
+                            f.exec();
+                        }
+                    }
+                });
             }
 
             @Override
             public void onTripleClick(int button) {
                 LogUtils.d("onTripleClick " + button);
-                IFunction f = getHomeTripleClickFunction(button);
-                if (f != null) {
-                    f.exec();
-                }
+                App.get().getHandler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        IFunction f = getHomeTripleClickFunction(button);
+                        if (f != null) {
+                            f.exec();
+                        }
+                    }
+                });
             }
         });
     }
@@ -315,7 +325,7 @@ public class AppEventManager implements KeyEventHandler.Callback {
         public void handle(int button) {
             if (lastButton == button) {
                 count++;
-                if (hasOnlyDoubleClick() && count == 2) {
+                if (hasOnlyDoubleClick(button) && count == 2) {
                     App.get().getHandler().removeMessages(WHAT_BUTTON_HANDLER);
                     if (callback != null) {
                         callback.onDoubleClick(lastButton);
@@ -348,8 +358,8 @@ public class AppEventManager implements KeyEventHandler.Callback {
             }
         }
 
-        public boolean hasOnlyDoubleClick() {
-            return false;
+        public boolean hasOnlyDoubleClick(int button) {
+            return getHomeTripleClickFunction(button) == null;
         }
 
         private void reset() {
