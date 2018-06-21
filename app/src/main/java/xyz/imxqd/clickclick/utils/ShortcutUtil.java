42777dfe45704edef128a4d5589a82b11644ff18
XuQidong <xuqidong@meizu.com>
2018-06-21 12:47:49 +0800

    get shortcuts by package name (only when ClickClick is default launcher)

--- app/src/main/java/xyz/imxqd/clickclick/utils/ShortcutUtil.java
+++ app/src/main/java/xyz/imxqd/clickclick/utils/ShortcutUtil.java
@@ -1,14 +1,25 @@
 package xyz.imxqd.clickclick.utils;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.LauncherApps;
+import android.content.pm.ShortcutInfo;
+import android.os.Build;
+import android.os.Process;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.pm.ShortcutInfoCompat;
 import android.support.v4.content.pm.ShortcutManagerCompat;
 import android.support.v4.graphics.drawable.IconCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import xyz.imxqd.clickclick.App;
 import xyz.imxqd.clickclick.BuildConfig;
 import xyz.imxqd.clickclick.R;
 import xyz.imxqd.clickclick.ui.NoDisplayActivity;
+
+import static android.content.pm.LauncherApps.ShortcutQuery.*;
 
 public class ShortcutUtil {
 
@@ -29,5 +40,20 @@ public class ShortcutUtil {
                 .build();
 
         ShortcutManagerCompat.requestPinShortcut(App.get(), infoCompat, null);
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    public static List<ShortcutInfo> getShortcuts(String packageName) {
+        LauncherApps launcherApps = (LauncherApps) App.get().getSystemService(Context.LAUNCHER_APPS_SERVICE);
+        LauncherApps.ShortcutQuery query = new LauncherApps.ShortcutQuery();
+        query.setQueryFlags(FLAG_MATCH_DYNAMIC | FLAG_MATCH_MANIFEST | FLAG_MATCH_PINNED);
+        query.setPackage(packageName);
+        try {
+            return launcherApps.getShortcuts(query, Process.myUserHandle());
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
