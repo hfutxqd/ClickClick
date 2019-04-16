99aac8261b6cf02859de2173fa339ba85f75aa5d
xuqd <xuqd@getui.com>
2019-04-16 13:54:53 +0800

    add LogcatUtil

---
+++ app/src/main/java/xyz/imxqd/clickclick/utils/LogcatUtil.java
@@ -0,0 +1,37 @@
+package xyz.imxqd.clickclick.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class LogcatUtil {
+    public static String getLogs() {
+        Process process = null;
+        try {
+            process = new ProcessBuilder()
+                    .command("logcat", "-d")
+                    .redirectErrorStream(true)
+                    .start();
+            StringBuilder builder = new StringBuilder();
+            InputStream in = process.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int readLen = 0;
+            char[] buffer = new char[1024];
+            do {
+                readLen = reader.read(buffer);
+                if (readLen > 0) {
+                    builder.append(buffer, 0, readLen);
+                }
+            } while (readLen > 0);
+            return builder.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        return null;
+    }
+}
